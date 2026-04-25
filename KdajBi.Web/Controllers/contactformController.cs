583c0ccc6813bccc57ac6e1ba3f93c86c6a26770
twooclock <twooclock@users.noreply.github.com>
2026-04-25 21:41:19 +0200

    contactform
    +added support for 3rd party contact form
    !appointment modal was showing only partial results for previous appointments

--- KdajBi.Web/Controllers/contactformController.cs
+++ KdajBi.Web/Controllers/contactformController.cs
@@ -14,7 +14,9 @@ namespace KdajBi.Web.Controllers
 {
     public class ContactForm
     {
+        public string code { get; set; }
         public string name { get; set; }
+        public string phone { get; set; }
         public string email { get; set; }
         public string subject { get; set; }
         public string message { get; set; }
@@ -39,11 +41,17 @@ namespace KdajBi.Web.Controllers
         [Route("/api/contactform")]
         public async Task<ActionResult<ContactForm>> contactform(ContactForm cForm)
         {
-
-            _logger.LogInformation(string.Format("New contact name:'{0}', email:'{1}', subject:'{2}', message:'{3}' ", cForm.name, cForm.email, cForm.subject, cForm.message));
-
-            await _emailSender.SendEmailAsync(cForm.email, _emailSender.emailSettings().AdminMail, "new contact!", string.Format("New contact name:'{0}', email:'{1}', subject:'{2}', message:'{3}' ", cForm.name, cForm.email, cForm.subject, cForm.message));
-
+            _logger.LogInformation(string.Format("New contact code:'{0}', name:'{1}', phone:'{2}', email:'{3}', subject:'{4}', message:'{5}' ", cForm.code, cForm.name, cForm.phone, cForm.email, cForm.subject, cForm.message));
+            switch (cForm.code)
+            {
+                case "ZATE":
+                    await _emailSender.SendEmailAsync(cForm.email, "salon.zate.info@gmail.com", "novo povpraševanje", string.Format("Ime:'{0}', Telefon:'{1}', ePošta:'{2}', sporočilo:'{3}' ", cForm.name, cForm.phone, cForm.email,  cForm.message));
+                    break;
+                default:
+                    await _emailSender.SendEmailAsync(cForm.email, _emailSender.emailSettings().AdminMail, "new contact!", string.Format("New contact name:'{0}', email:'{1}', subject:'{2}', message:'{3}' ", cForm.name, cForm.email, cForm.subject, cForm.message));
+                    break;
+            }
+            
             return Json("OK");
         }
     }
